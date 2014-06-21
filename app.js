c3206fa514e6b10fc98dcdc1ae56d6be5942f511
Raymond Jacobson <me@raymondjacobson.com>
2014-06-21 14:59:09 -0700

    build basic node framework

---
+++ app.js
@@ -0,0 +1,33 @@
+var express = require('express')
+  , http = require('http')
+  , stylus = require('stylus')
+  , nib = require('nib')
+
+var app = express();
+function compile(str, path) {
+  return stylus(str)
+    .set('filename', path)
+    .use(nib());
+}
+app.set('port', process.env.PORT || 7486);
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.logger('dev'));
+app.use(stylus.middleware(
+  { src: __dirname + '/assets'
+  , compile: compile
+  }
+));
+app.use(express.static(__dirname + '/assets'))
+
+
+app.get('/', function (req, res) {
+  res.render('index',
+  { title: '' }
+  )
+});
+
+http.createServer(app).listen(app.get('port'), function () {
+  console.log('Express server listening on port ' + app.get('port'));
+});
