cd7705553f8f03d35f848b49b89af90d38548792
Raymond Jacobson <me@raymondjacobson.com>
2014-06-21 17:32:25 -0700

    add db drives & python call

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
+var db = require('../lib/db.js');
+var caller = require('../lib/caller.js');
+
 exports.index = function(req, res){
-
-  var Firebase = require('firebase')
-
-  var myRootRef = new Firebase('https://cflo-sbd.firebaseio.com/');
-
-  myRootRef.set("hellof world!");
+  // Set up firebase connection
+  db_url = db.getDBUrl();
+  db.establishConnection(db_url);
+  caller.processData();
   console.log('here');
   res.render('index', { title: 'Express' });
-
 };
